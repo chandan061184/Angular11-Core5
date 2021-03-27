46043ff4fb6d771eaafe45ae34d7072a32b7eeb3
Chandan Kumar <chandan061184@outlook.com>
2021-03-27 18:54:49 +0530

    Added material toolbar as menu

--- angular-movies/src/app/material/material.module.ts
+++ angular-movies/src/app/material/material.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import{ MatToolbarModule} from '@angular/material/toolbar'
+import{ MatButtonModule} from '@angular/material/button'
+import{ MatIconModule} from '@angular/material/icon'
 
 @NgModule({
   declarations: [],
+  exports:[
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   imports: [
     CommonModule
   ]
